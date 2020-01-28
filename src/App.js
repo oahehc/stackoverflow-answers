12598fdcca1ea2d0fa90e6ff93b7832f6cd0f325
oahehc <oahehc@gmail.com>
2020-01-28 17:54:46 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+function CheckBox({ value, label, isChecked }) {
+  return (
+    <label>
+      <input type="checkbox" checked={isChecked} value={value} />
+      <span>{label}</span>
+    </label>
+  );
+}
 
 function App() {
+  // list of checkbox, `value` should be the format we want to save, label is something we want to display to the users
+  const list = [
+    { value: "1100", label: "11:00" },
+    { value: "1200", label: "12:00" },
+    { value: "1300", label: "13:00" }
+  ];
+  // every item default is un-checked
+  const [data, setData] = useState(() =>
+    list.map(item => ({
+      ...item,
+      isChecked: false
+    }))
+  );
+
+  // update the check status in the parent component
+  function handleToggleCheckbox(value) {
+    setData(list =>
+      list.map(item => {
+        if (item.value === value) {
+          return {
+            ...item,
+            isChecked: !item.isChecked
+          };
+        }
+        return item;
+      })
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.map(({ value, label, isChecked }) => (
+        <CheckBox
+          key={value}
+          checked={isChecked}
+          value={value}
+          label={label}
+          onClick={() => handleToggleCheckbox(value)}
+        />
+      ))}
     </div>
   );
 }
