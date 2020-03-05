a3bd2010f56ae1b7574c52e0aff8c7e5f0775801
oahehc <oahehc@gmail.com>
2020-03-05 09:10:18 +0800

    Feature: test for asyc custom hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from "react";
-import "./App.css";
 
+export const useCustomHook = (id) => {
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState(false);
+  const [value, setValue] = React.useState();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  React.useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((res) => {
+        if (res.status === 200) return res.json()
+        throw new Error();
+      })
+      .then((data) => {
+        setValue(data.message);
+      }).catch((err) => {
+        setError(true)
+      }).finally(() => {
+        setLoading(false);
+      })
+  }, [id]);
+
+  return [loading, error, value];
+};
+
+const App = () => {
+  const [num, setNum] = React.useState(1);
+  const [loading, error, value] = useCustomHook(num);
+
+  if (loading) { return <div>Loading...</div>; }
+  if (error) { return <div>Error</div>; }
+  return <div><input value={num} onChange={(e) => setNum(e.target.value)} /><img src={value} alt="dog" /></div>;
+};
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,5 +1,58 @@
-describe('test', () => {
-  it('should pass', () => {
-    expect(true).toBe(true);
+import React from 'react'
+import { act } from 'react-dom/test-utils';
+import App from '../App'
+import { mount } from 'enzyme';
+
+describe('App', () => {
+  beforeEach(() => {
+    fetch.resetMocks();
+  });
+
+  it('should fetch the request', async () => {
+    await act(async () => {
+      await mount(<App />)
+    })
+
+    expect(fetch).toBeCalledWith('https://dog.ceo/api/breeds/image/random');
+  });
+
+  it('should show loading at first', () => {
+    jest
+      .spyOn(React, 'useEffect')
+      .mockImplementationOnce(() => {}); 
+
+    const comp = mount(<App />)
+
+    expect(comp.text()).toBe('Loading...');
+  });
+
+  it('should display error if api request fail', async () => {
+    fetch.mockRejectOnce();
+    let comp;
+
+    await act(async () => {
+      comp = await mount(<App />);
+    })
+    comp.update();
+
+    expect(comp.text()).toBe('Error');
+  });
+
+  it('should display result if api request success', async () => {
+    fetch.mockResponseOnce(JSON.stringify({
+      message: "https://images.dog.ceo/breeds/mastiff-tibetan/n02108551_1287.jpg",
+      status: "success"
+    }));
+    let comp;
+
+    await act(async () => {
+      comp = await mount(<App />);
+    })
+    comp.update();
+
+
+    expect(comp.find('img')).toHaveLength(1);
+    expect(comp.find('img').prop('src'))
+      .toBe('https://images.dog.ceo/breeds/mastiff-tibetan/n02108551_1287.jpg');
   });
 });
